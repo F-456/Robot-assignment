4358ea4d566969630973ab29239ed984c5f5eb0f
nbzy05@gmail.com <nbzy05@gmail.com>
2025-04-28 21:17:00 +0800

    Nicholas comit

--- main.cpp
+++ main.cpp
@@ -1,19 +1,9 @@
-/**********|**********|**********|
-Program: YOUR_FILENAME.cpp / YOUR_FILENAME.h
-Course: Data Structures and Algorithms
-Trimester: 2410
-Name: Frank Carrano
-ID: 1071001234
-Lecture Section: TC101
-Tutorial Section: TT1L
-Email: abc123@yourmail.com
-Phone: 018-1234567
-**********|**********|**********/
 #include <iostream>
 #include <fstream>
 #include <sstream>
-using namespace std;
 #include "function.h"
+
+using namespace std;
 
 int main()
 {
